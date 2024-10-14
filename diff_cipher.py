6719988a9b4a170d4552924deec99b69229dbe5c
EthanA-LCWC <144792227+EthanA-LCWC@users.noreply.github.com>
2024-10-14 13:10:05 +0000

    Can't think of a message

--- diff_cipher.py
+++ diff_cipher.py
@@ -1,1 +1,28 @@
-
+def dif_ciph(words):
+    first_loop = True
+    if type(words) == str:
+        new_words = []
+        for item in words:
+            item = ord(item)
+            if first_loop == True:
+                new_item = item
+                first_loop = False
+            else:
+                new_item = item - last_item
+            last_item = item
+            new_words.append(new_item)
+        return new_words
+    else:
+        new_string = ""
+        for item in words:
+            if first_loop == True:
+                item = chr(item)
+                new_item = item
+                last_item = ord(item)
+                first_loop = False
+            else:
+                new_item = item + last_item
+                new_item = chr(new_item)
+                last_item = ord(new_item)
+            new_string += new_item
+        return new_string
